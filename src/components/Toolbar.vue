9fbc3edd29df6c3b7ba79b7bf46959c5ee0f8adf
SakilaWAW <pccp0909@163.com>
2017-08-11 08:58:44 +0800

    用flex布局代替原来的手动百分比margin

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -4,7 +4,7 @@
       <li>this is header</li>
     </ul>
     <ul class="tg-ac-list">
-      <li class="tg-ac-item" ><img class="tg-ac-tuco" :src="img" alt="site logo named Tuco"></li>
+      <li class="tg-ac-item tg-ac-tuco" ><img :src="img" alt="site logo named Tuco"></li>
       <li class="tg-ac-item tg-ac-aboutme">关于我</li>
       <li class="tg-ac-item tg-ac-java">Java</li>
       <li class="tg-ac-item tg-ac-python">Python</li>
@@ -29,7 +29,46 @@ export default {
 <style lang="scss" scoped>
 $light-pink: #e2627d;
 
-nav.nav-bar {
+.nav-bar {
+  background: whiteSmoke;
+  padding: 15px 0;
+  .tg-ac-header {
+    @media screen and (min-width: 768px){
+      display: none;
+    }
+  }
+  .tg-ac-list{
+    margin: 0 auto;
+    padding: 0 22px 0 0;
+    width: 750px;
+    display: flex;
+    list-style-type: none;
+    align-items: center;
+    @media screen and (max-width: 767px){
+      display: none;
+    }
+  }
+  .tg-ac-item {
+    text-align: center;
+    flex: 1;
+    box-sizing: border-box;
+    font-weight: bold;
+    font-family: Helvetica, Tahoma, Arial, STXihei, "华文细黑", Heiti, "黑体", "Microsoft YaHei", "微软雅黑", SimSun, "宋体", sans-serif;
+    color: $light-pink;
+    &:hover {
+      color: midnightBlue;
+      cursor: pointer;
+    }
+  }
+  .tg-ac-tuco {
+    flex: 1.5;
+  }
+}
+
+/**
+ * 不用flexbox的写法
+ */
+/**
   background: whiteSmoke;
   padding: 15px 0;
   .tg-ac-header {
@@ -59,4 +98,5 @@ nav.nav-bar {
     }
   }
 }
+**/
 </style>
