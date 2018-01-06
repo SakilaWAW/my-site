6b43ff011ea1891802529ff1909543957dc3a5c7
sakilawaw <pccp0909@163.com>
2018-01-06 23:24:46 +0800

    尝试使用ElementUI库完成卡片demo 并设置about me模块article-zone宽度

--- src/components/sitecontent/aboutMeContent.vue
+++ src/components/sitecontent/aboutMeContent.vue
@@ -1,6 +1,12 @@
 <template lang="html">
   <div class="about-me-content">
-    <i>about-me-content</i>
+    <div class="article-zone">
+      <el-card class="box-card">
+        <div v-for="o in 4" :key="o" class="text item">
+          {{'列表内容 ' + o}}
+        </div>
+      </el-card>
+    </div>
   </div>
 </template>
 
@@ -11,4 +17,23 @@ export default {
 </script>
 
 <style lang="scss">
+  .article-zone {
+    border: 2px solid purple;
+    margin: auto;
+    @media screen and (min-width: 900px) {
+      width: 900px;
+    }
+    @media screen and (max-width: 899px) {
+      width: 100%;
+    }
+  }
+  .text {
+    font-size: 14px;
+  }
+  .item {
+    padding: 18px 0;
+  }
+  .box-card {
+    width: 480px;
+  }
 </style>
